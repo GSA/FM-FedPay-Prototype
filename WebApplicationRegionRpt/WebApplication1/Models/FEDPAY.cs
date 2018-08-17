967b72c5635b9273cbebf0807bf664dd92c5a1d2
Brad <brad.powers@gsa.gov>
2018-08-17 11:33:19 -0500

    New clean code as of 8-17 11:30am

---
+++ WebApplicationRegionRpt/WebApplication1/Models/FEDPAY.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using FEDPAY.Models;
+
+namespace FEDPAYmgr.Models
+{
+    /// <summary>
+    /// Define FEDPAY Context and tables in the Context
+    /// </summary>
+    public class FEDPAYContext : DbContext
+    {
+        public FEDPAYContext(DbContextOptions<FEDPAYContext> options)
+            : base(options) { }
+
+
+        public DbSet<FEDPAY.Models.Region> Region { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ///
+            /// Assign default database schema.  
+            /// The database service has limited system access right to create objects in the sift schema
+            /// 
+            modelBuilder.HasDefaultSchema("fedpay");
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ WebApplicationRegionRpt/WebApplication1/Models/Region.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FEDPAY.Models
+{
+    /// <summary>
+    /// FEDPAY MISC.REGION table.  
+    /// </summary>
+    public class Region
+    {
+        [Key]
+        public string REG_PO_SUFFIX { get; set; }
+        public string REG_FSS_REGION_CODE { get; set; }
+        public string REG_FAIM_FB_REGION_CODE { get; set; }
+        public string REG_FEDPAY_REGION_CODE { get; set; }
+        public decimal REG_ROUTING_ID { get; set; }
+        public string REG_INFONET_REGION_CODE { get; set; }
+        
+
+    }
+}
