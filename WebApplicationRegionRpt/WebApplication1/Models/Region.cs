65d5fb1d48a882c0631fa255ebd74c2148e11407
Brad <brad.powers@gsa.gov>
2018-09-21 14:58:05 -0500

    Finalized Draft Report

--- WebApplicationRegionRpt/WebApplication1/Models/Region.cs
+++ WebApplicationRegionRpt/WebApplication1/Models/Region.cs
@@ -8,11 +8,22 @@ namespace FEDPAY.Models
     public class Region
     {
         [Key]
+        [Display(Name = "PO Suffix")]
         public string REG_PO_SUFFIX { get; set; }
+
+        [Display (Name = "FAS Region")]
         public string REG_FSS_REGION_CODE { get; set; }
+
+        [Display(Name = "FAIM FEDBIL Region")]
         public string REG_FAIM_FB_REGION_CODE { get; set; }
+
+        [Display(Name = "FEDPAY Region")]
         public string REG_FEDPAY_REGION_CODE { get; set; }
+
+        [Display(Name = "Routing ID")]
         public string REG_ROUTING_ID { get; set; }
+
+        [Display(Name = "NEAR Region")]
         public string REG_INFONET_REGION_CODE { get; set; }
         
 
